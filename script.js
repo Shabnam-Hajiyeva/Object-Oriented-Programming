749c766846465ea3b448d69a8f9aee7c4bef0fd6
Shebnem Hajiyeva <hajiyevashebnem@gmail.com>
2025-08-16 16:02:16 -0400

    AddedfindProductByName method

--- script.js
+++ script.js
@@ -66,7 +66,9 @@ addProduct(product) {
   getInventoryValue() {
     return this.inventory.reduce((total, product) => total + product.getTotalValue(), 0);
   }
-
+findProductByName(name) {
+    return this.inventory.find(product => product.name === name) || null;
+  }
 }
 
 
