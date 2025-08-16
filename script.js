13168611b8fcf06f42a85814935bed227ddfd7aa
Shebnem Hajiyeva <hajiyevashebnem@gmail.com>
2025-08-16 14:22:49 -0400

    Added ProductProperties class

---
+++ script.js
@@ -0,0 +1,24 @@
+class  ProductProperties{
+    constructor (name,price,quantity){
+        this.name=name;
+    this.price=price
+    this.quantity=quantity;
+
+    }
+
+    getTotalValue(){
+        return this.price*this.quantity;
+    }
+toString(){
+    return `Product: $${this.name} Price: $${this.price} Quantity: $${this.quantity} `;
+
+}
+}
+let product=new ProductProperties("Apple", 2.50, 50);
+console.log(product.getTotalValue());
+console.log(product.toString());
+
+
+
+
+    
